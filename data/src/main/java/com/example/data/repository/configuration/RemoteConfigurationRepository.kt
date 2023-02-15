26ee3f787f7ab30048193d26a850c80afb011531
Ivan Kitaev <ivanbestboy@gmail.com>
2023-02-15 15:53:00 +0400

    missing changes

--- data/src/main/java/com/example/data/repository/configuration/RemoteConfigurationRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.data.repository.configuration
-
-interface RemoteConfigurationRepository {
-
-    suspend fun getPosterImageBaseUrl(): String
-
-    suspend fun getOriginalImageBaseUrl(): String
-}
