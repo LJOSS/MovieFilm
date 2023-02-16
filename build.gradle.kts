addca00c043e353cc0d325ad5cd0498915c5e473
Ivan Kitaev <ivanbestboy@gmail.com>
2023-02-16 13:09:03 +0400

    remove jcent

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,3 @@
-buildscript {
-    repositories {
-        google()
-        maven("https://plugins.gradle.org/m2/")
-        mavenCentral()
-    }
-    dependencies {
-        classpath(AppDependencies.gradle)
-        classpath(AppDependencies.Kotlin.kotlin)
-        classpath(AppDependencies.AndroidX.navigationSafeArgs)
-        classpath(AppDependencies.Plugins.kotlinterPlugin)
-    }
-}
-
 allprojects {
     repositories {
         google()
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -4,7 +4,15 @@ plugins {
     `kotlin-dsl`
 }
 
-@Suppress("JcenterRepositoryObsolete")
 repositories {
-    jcenter()
+    google()
+    maven("https://plugins.gradle.org/m2/")
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.android.tools.build:gradle:7.3.1")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
+    implementation("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.3")
+    implementation("org.jmailen.gradle:kotlinter-gradle:3.13.0")
 }
